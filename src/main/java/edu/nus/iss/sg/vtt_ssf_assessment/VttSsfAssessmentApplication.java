02803a52a8596fa7eebee80a7262430a830e3dcc
unknown <marcuskhoo2@gmail.com>
2022-03-25 09:13:19 +0800

    initial cmmit

---
+++ src/main/java/edu/nus/iss/sg/vtt_ssf_assessment/VttSsfAssessmentApplication.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.sg.vtt_ssf_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttSsfAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttSsfAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nus/iss/sg/vtt_ssf_assessment/VttSsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.sg.vtt_ssf_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttSsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
