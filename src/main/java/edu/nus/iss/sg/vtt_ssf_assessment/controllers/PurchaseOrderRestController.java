fc57f6f2e51d59c8f75b408c8ec667afe0551efd
unknown <marcuskhoo2@gmail.com>
2022-03-25 10:45:45 +0800

    update functionalities

---
+++ src/main/java/edu/nus/iss/sg/vtt_ssf_assessment/controllers/PurchaseOrderRestController.java
@@ -0,0 +1,70 @@
+package edu.nus.iss.sg.vtt_ssf_assessment.controllers;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Map.Entry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.nus.iss.sg.vtt_ssf_assessment.models.Quotation;
+import edu.nus.iss.sg.vtt_ssf_assessment.services.QuotationService;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+
+@RestController
+@RequestMapping(path="/api/po")
+public class PurchaseOrderRestController {
+
+    @Autowired
+    private QuotationService quotationSvc;
+    
+    @PostMapping(path="", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> postPurchaseOrder(@RequestBody String payload) {
+        
+        InputStream is = new ByteArrayInputStream(payload.getBytes());
+        JsonReader reader = Json.createReader(is);
+        JsonObject poObj = reader.readObject();
+        
+        String name = poObj.getString("name");
+        JsonArray listItems = poObj.getJsonArray("lineItems");
+        Map<String, Integer> itemMap = new HashMap<>();
+
+        for(int i = 0; i < listItems.size(); i++) {
+            JsonObject item = listItems.getJsonObject(i);
+            itemMap.put(item.getString("item"), item.getInt("quantity"));
+        }
+  
+        Optional<Quotation> quotationOpt = quotationSvc.getQuotations(new ArrayList<>(itemMap.keySet()));
+
+        if(quotationOpt.isEmpty()) {
+            return ResponseEntity.badRequest().body("{}");
+        }
+
+        Quotation quotation = quotationOpt.get();
+        Float total = 0.0f;
+        for(Entry<String, Integer> entry : itemMap.entrySet()) {
+            Float unitPrice = quotation.getQuotation(entry.getKey());
+            total += entry.getValue() * unitPrice;
+        }
+
+        JsonObject jObj = Json.createObjectBuilder()
+                                .add("invoiceId", quotation.getQuoteId())
+                                .add("name", name)
+                                .add("total", total)
+                                .build();
+
+        return ResponseEntity.ok().body(jObj.toString());
+    }
+}
--- src/test/java/edu/nus/iss/sg/vtt_ssf_assessment/VttSsfAssessmentApplicationTests.java
+++ src/test/java/edu/nus/iss/sg/vtt_ssf_assessment/VttSsfAssessmentApplicationTests.java
@@ -1,13 +1,34 @@
 package edu.nus.iss.sg.vtt_ssf_assessment;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import edu.nus.iss.sg.vtt_ssf_assessment.models.Quotation;
+import edu.nus.iss.sg.vtt_ssf_assessment.services.QuotationService;
 
 @SpringBootTest
 class VttSsfAssessmentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private QuotationService quotationSvc;
+
+    @Test
+    void contextLoads() {
+        List<String> names = new ArrayList<>(){{
+                                    add("durian");
+                                    add("plum");
+                                    add("pear"); 
+                                }};
+        
+        Optional<Quotation> quotationOpt = quotationSvc.getQuotations(names);
+
+        Assertions.assertTrue(quotationOpt.isPresent());
+    }
 
 }
